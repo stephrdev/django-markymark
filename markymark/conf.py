13eb984a52b6f7459e7fbcbb33b50d8747859439
Christopher Grebs <cg@webshox.org>
2015-01-27 10:50:18 +0100

    Disable anylink and filer integration per default.

--- markymark/conf.py
+++ markymark/conf.py
@@ -3,15 +3,12 @@ from django.conf import settings
 
 MARKYMARK_EXTENSIONS = getattr(settings, 'MARKYMARK_EXTENSIONS', [
     'markymark.extensions:LinkExtension',
-    'markymark.extensions:FilerFileExtension',
-    'markymark.extensions:AutoLinkExtension',
 ])
 
 
 MARKYMARK_CSS = getattr(settings, 'MARKYMARK_CSS', [
     'markdown/css/markdown-editor.css',
     'markdown/css/markdown-editor-adminfix.css',
-    'markdown/css/plugins/filer-file.css',
 ])
 
 
@@ -19,7 +16,5 @@ MARKYMARK_JS = getattr(settings, 'MARKYMARK_JS', [
     'markdown/js/markdown-init.js',
     'markdown/js/markdown.js',
     'markdown/js/markdown-editor.js',
-    'markdown/js/plugins/clean.js',
-    'markdown/js/plugins/anylink-link.js',
-    'markdown/js/plugins/filer-file.js',
+    'markdown/js/plugins/clean.js'
 ])
