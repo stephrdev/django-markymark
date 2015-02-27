38937b6c2bff0c1bb8d76cedbcc56dcc109fcba3
Christopher Grebs <cg@webshox.org>
2015-02-27 14:53:24 +0100

    Avoid name clashes in anylink module.

--- markymark/extensions/contrib/anylink.py
+++ markymark/extensions/contrib/anylink.py
@@ -11,12 +11,12 @@ import markdown
 LINK_RE = re.compile(r'(\[link\:(?P<id>\d+)\])', re.IGNORECASE)
 
 
-class LinkExtension(markdown.Extension):
+class AnyLinkExtension(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
-        md.postprocessors.add('link', LinkPostprocessor(md), '_end')
+        md.postprocessors.add('anylink', LinkPostprocessor(md), '_end')
 
 
-class LinkPostprocessor(markdown.postprocessors.Postprocessor):
+class AnyLinkPostprocessor(markdown.postprocessors.Postprocessor):
     def run(self, text):
         def re_callback(match):
 
@@ -25,7 +25,7 @@ class LinkPostprocessor(markdown.postprocessors.Postprocessor):
                 link = AnyLink.objects.get(pk=int(options['id']))
                 return match.group(0).replace(
                     match.group(1),
-                    render_to_string('markdown/link.html', {'link': link})
+                    render_to_string('markdown/anylink.html', {'link': link})
                 )
 
             except (KeyError, AnyLink.DoesNotExist):
