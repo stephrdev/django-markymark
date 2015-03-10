c072d136bbfe99504d2b264ed67e9f79abd15d13
Christopher Grebs <cg@webshox.org>
2015-03-10 12:18:01 +0100

    Fix tests for recent changes

--- markymark/tests/test_widgets.py
+++ markymark/tests/test_widgets.py
@@ -6,13 +6,12 @@ class TestMarkdownTextarea:
     def test_attrs(self):
         rendered = MarkdownTextarea().render('testfield', '')
         assert '<textarea' in rendered
-        assert 'data-provide="init-markdown"' in rendered
+        assert 'data-provide="markdown"' in rendered
 
     def test_media(self):
         widget = MarkdownTextarea()
         assert 'markdown/js/markdown.js' in widget.media._js
         assert 'markdown/js/markdown-editor.js' in widget.media._js
-        assert 'markdown/js/markdown-init.js' in widget.media._js
         assert 'markdown/js/plugins/clean.js' in widget.media._js
         assert 'markdown/js/plugins/anylink-link.js' in widget.media._js
         assert 'markdown/js/plugins/filer-file.js' in widget.media._js
